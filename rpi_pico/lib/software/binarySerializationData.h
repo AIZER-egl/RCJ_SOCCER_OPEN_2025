d694914a217ab11730083f6595574c3eb0af2154
IkerCs <iker.cossio.garcia@gmail.com>
2025-04-17 23:32:46 -0600

    USB communication with serialization instead of bitmask

---
+++ rpi_pico/lib/software/binarySerializationData.h
@@ -0,0 +1,39 @@
+#ifndef BINARYSERIALIZATIONDATA_H
+#define BINARYSERIALIZATIONDATA_H
+
+#include <cstdint>
+#include <cstddef>
+
+#pragma pack(push, 1)
+
+struct BinarySerializationData {
+	uint8_t motor_se_speed;
+	uint8_t motor_sw_speed;
+	uint8_t motor_ne_speed;
+	uint8_t motor_nw_speed;
+
+	uint8_t motor_se_rpm;
+	uint8_t motor_sw_rpm;
+	uint8_t motor_ne_rpm;
+	uint8_t motor_nw_rpm;
+
+	uint8_t motor_se_direction;
+	uint8_t motor_sw_direction;
+	uint8_t motor_ne_direction;
+	uint8_t motor_nw_direction;
+
+	uint16_t compass_yaw;
+
+	bool kicker_active;
+
+	uint8_t ldr_value;
+
+	uint8_t setting_team_id;
+	uint8_t setting_attack_goal;
+};
+
+#pragma pack(pop)
+
+const size_t BINARY_SERIALIZATION_DATA_SIZE = sizeof(BinarySerializationData);
+
+#endif //BINARYSERIALIZATIONDATA_H
---
+++ rpi_pico/lib/software/serializer.h
@@ -0,0 +1,15 @@
+#ifndef PICO_LIB_BINARY_SERIALIZATION_H
+#define PICO_LIB_BINARY_SERIALIZATION_H
+
+#include "BinarySerializationData.h"
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+
+namespace BinarySerialization {
+public:
+	BinarySerialization() = default;
+
+}
+
+#endif //PICO_LIB_BINARY_SERIALIZATION_H
